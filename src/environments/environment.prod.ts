f96651bb29072adf757de76d5b51fe60726fed55
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2025-02-20 15:49:17 -0500

    feat: add new environment for PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   APP_NAME: 'Atom TODO prod',
   APP_DESCRIPTION: 'Aplicación de tareas con Angular 18 para gestionar tareas',
+  API_URL: 'https://us-central1-prueba-test-atom.cloudfunctions.net/api'
 };
